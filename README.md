61540dbc0b9008fc77dc816047f937305f0b0ed5
telco2011 <davidlopez.david@gmail.com>
2016-02-04 10:13:43 +0100

    Update README and gulpfile with help utilities

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ To run
 
 To run with gulp
 - gulp
+
+**RECOMENDATION**: use node 4.2.6 version.
--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
-var gulp = require('gulp');
+var os = require('os');
+var gulp = require('gulp-help')(require('gulp'));
 var jshint = require('gulp-jshint');
 var jshintReporter = require('jshint-stylish');
 var watch = require('gulp-watch');
-var shell = require('gulp-shell')
-
+var shell = require('gulp-shell');
+var open = require('gulp-open');
 var sass = require('gulp-sass');
-
+var wait = require('gulp-wait')
+var runSequence = require('run-sequence');
 
 var paths = {
 	'src':['./models/**/*.js','./routes/**/*.js', 'keystone.js', 'package.json']
@@ -18,8 +20,12 @@ var paths = {
 
 };
 
+var browser = os.platform() === 'linux' ? 'google-chrome' : (
+	          os.platform() === 'darwin' ? 'google chrome' : (
+			  os.platform() === 'win32' ? 'chrome' : 'firefox'));
+
 // gulp lint
-gulp.task('lint', function(){
+gulp.task('lint', 'Lints all server side', function(){
 	gulp.src(paths.src)
 		.pipe(jshint())
 		.pipe(jshint.reporter(jshintReporter));
@@ -35,14 +41,19 @@ gulp.task('watch:sass', function () {
 	gulp.watch(paths.style.all, ['sass']);
 });
 
-gulp.task('sass', function(){
+gulp.task('sass', 'Compile sass.', function(){
 	gulp.src(paths.style.all)
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest(paths.style.output));
 });
 
+gulp.task('runKeystone', 'Keystone instance up.', shell.task('node keystone.js'));
 
-gulp.task('runKeystone', shell.task('node keystone.js'));
+gulp.task('openBrowser', 'Open browser with default URL to access.', function(){
+	var config = { app: browser, uri: 'http://localhost:3000'};
+	gulp.src('').pipe(open(config));
+});
+
 gulp.task('watch', [
 
   'watch:sass',
@@ -50,4 +61,6 @@ gulp.task('watch', [
   'watch:lint'
 ]);
 
-gulp.task('default', ['watch', 'runKeystone']);
+gulp.task('default', function(cb){
+	runSequence('watch', 'runKeystone', cb);
+});
