f4ec864f6e161600e27595b3597e25f78e412e34
Roberto Zazo <roberto.zazo.lopez@everis.com>
2016-03-10 16:42:17 +0100

    Communities | Update information of communities

--- updates/0.0.1-8-communities.js
+++ updates/0.0.1-8-communities.js
@@ -5,21 +5,21 @@ exports.create = {
 			description: "Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine. Node.js uses an event-driven, non-blocking I/O model that makes it lightweight and efficient. " +
 			"Node.js' package ecosystem, npm, is the largest ecosystem of open source libraries in the world....",
 			logosrc: "nodejs.png",
-			link: "/blog/"
+			link: "/community"
 		},
 		{
 			name: "NodeJS",
 			description: "Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine. Node.js uses an event-driven, non-blocking I/O model that makes it lightweight and efficient. " +
 			"Node.js' package ecosystem, npm, is the largest ecosystem of open source libraries in the world....",
 			logosrc: "nodejs.png",
-			link: "/blog/"
+			link: "/community"
 		},
 		{
 			name: "NodeJS",
 			description: "Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine. Node.js uses an event-driven, non-blocking I/O model that makes it lightweight and efficient. " +
 			"Node.js' package ecosystem, npm, is the largest ecosystem of open source libraries in the world....",
 			logosrc: "nodejs.png",
-			link: "/blog/"
+			link: "/community"
 		}
 	]
 };
