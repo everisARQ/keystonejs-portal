7d98dff644d5a2f9f7aecf02ab8d6af2457abcff
Roberto Zazo <roberto.zazo.lopez@everis.com>
2016-03-10 17:57:05 +0100

    Community | Add title community by paremeter into url

--- routes/views/community.js
+++ routes/views/community.js
@@ -17,20 +17,23 @@ exports = module.exports = function(req, res) {
 	locals.section = 'community';
 	locals.data = {
 		communities: [],
-		comunity : []
+		community : []
 	};
 
 	view.on('init', function(next) {
-		console.log("req.params.communityKey > "+req.params.communityKey);
 		if (req.params.communityKey) {
 			
-			keystone.list('Community').model.findOne({ key: locals.filters.communityKey }).exec(function(err, result) {
+			console.log("locals.filters.communityKey : "+locals.filters.communityKey );
+			
+			keystone.list('Community').model.find({ 'key': locals.filters.communityKey }).exec(function(err, result) {
+				
 				if (err || !result.length) {
+					console.log("Error : "+err);
 					return next(err);
 				}
 				
-				locals.data.comunity = result;
-				console.log("locals.data.comunity | "+ locals.data.comunity.name);
+				locals.data.community = result[0];
+				console.log("locals.data.comunity | "+ locals.data.community.name);
 				
 				next();
 			});
